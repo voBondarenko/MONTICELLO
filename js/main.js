cf613b195fe2644a542d9a1254961be7ad049467
voBondarenko <vlad.o.bondarenko@gmail.com>
2021-06-23 18:18:54 +0300

    v 1.3

--- js/main.js
+++ js/main.js
@@ -32,34 +32,6 @@ $(document).ready(function() {
                   slidesToShow: 2
                 }
             }
-            // ,
-            // {
-            //   breakpoint: 992,
-            //   settings: {
-            //     arrows: false,
-            //     centerMode: true,
-            //     centerPadding: '0px',
-            //     slidesToShow: 2
-            //   }
-            // },
-            // {
-            //   breakpoint: 768,
-            //   settings: {
-            //     arrows: false,
-            //     centerMode: true,
-            //     centerPadding: '0px',
-            //     slidesToShow: 1
-            //   }
-            // },
-            // {
-            //     breakpoint: 480,
-            //     settings: {
-            //         arrows: false,
-            //         centerMode: true,
-            //         centerPadding: '0px',
-            //         slidesToShow: 1
-            //     }
-            //   }
           ]
         });
 })
@@ -101,4 +73,23 @@ function openWindow(classParent) {
     
 }
 
+function openNews(nNews){
 
+    let news = document.querySelector("#news")
+    if(!document.querySelector(".newPNews")) {
+        let newPNews = document.createElement("p");
+        if(nNews==1){
+            newPNews.innerHTML = "Free directories: directories are perfect for customers that are... searching for a particular topic. What’s great about them is that you only have to post once and they are good for long periods of time. It saves a lot of your time when you don’t have to resubmit your information every week or every month. The bad news is most of your traffic won’t come from here. I still feel it is worth it to get your link out there. Just take one day and set it aside for posting to free directories. You won’t need to do it again for at least 6 months.2. Classified Ads: These are great for work from home businesses. Think about it. Where do people go when they are looking for a job? That’s right-the classifieds. The only downside to classified ads is that you have to resubmit them quite frequently. Once you find which classifieds bring you the most traffic you can concentrate on them and weed the others out. So it is really more time consuming in the beginning, and doesn’t have to be later on when you get the hang of things.3. Free article submissions/ezines: The best way to inform others about your product or service is to write an article about it. In your authors resource box, you can tell readers about yourself and where they can go to check out your product or service. This is also an excellent way to get free links to your site if you have one. There are a lot of webmasters out there who are looking for good articles they can post on their site. If they post yours, that is another site that is doing the advertising for you. All for free.4. URL Submissions: Probably the quickest and easiest thing to do to advertise your site. Just type “Free URL submission” in your search engine. When you get a list just enter the URL you are promoting and click submit. That’s it. It only takes a few seconds and your done. Just set a day aside once every 3 months and do this.5. Forum Posts: Put your product or service website in your signature file when you sign up on some forums. It will be displayed every time you make a post. Try to look for topics that you have some knowledge on, and can give a relevant answer too. Do not spam anyone, you will get kicked off the forum and you will get a bad reputation. Get involved asking and answering questions that pertain to your area of business. Forums are great because once you make a post it stays there forever. It will get moved to the archives eventually, but someone could still find it if they were searching the archives. Yes, there are many people who do.6. Traffic Exchanges: Probably the most time consuming way to advertise for free, but definately the most effective. Most forums I have visited have said in many posts that they received a lot of their profit from traffic exchanges. If you don’t want to spend the time surfing for credits, you do have the option of buying them. I would look into a program that lets you surf multiple websites at once like crazy browser. There are others and they are free to download. That way you can just spend one hour a day and get all your surfing in at once.7. News-groups: Become involved in a group that has to do with your kind of business. You can usually mail the group once per day, but I would encourage you to find something fresh to talk about each day. People will tune you out if they see the same message all the time. Remember to never Spam anyone. Only join groups with the same interests as yours. In other words, don’t sign up for a recipe swapping group when you are advertising shaving cream.";
+        }else if(nNews==2){
+            newPNews.innerHTML = "Having a home based business is a wonderful asset to your life...The problem still stands, when it comes time to advertise your business for a cheap cost. I know you have looked for this answer everywhere; I am here to share a few simple yet creative ways, to advertise your new home business. A few suggestions would be, newspapers is the one source that many people go to, when they have just opened there business. You would need to research online, for newspapers that are in your area. Newspapers are a very simple yet effective tool to use to take your business from basic home based business to business galore. Having catchy phrases and pictures or photographs of your products should help spice it up. Knowing your audience is the key to businesses. There are also, radio stations you could call in, or even email too. If you are a communicator, you can try to get onto a radio station and just share your business. You would need to write an outline or some type of catchy jingle that will help you be different from the rest of the business. The creativity comes from within. Knowing your target and knowing what you provide is gold, will surely, make you stand out from the rest of the crowd. Did you know that creativity comes from the heart and soul in once you find the creative voice you will find all kinds of ways to advertise your home business without spending a fortune for a market rep when you can do it yourself saving yourself time and money where you can be spending it on something else.You may also want to try in write a catchy jingle that will catch your readers and target audience. Being creative with your jingle will sure bring you visitors to your business rather it’s a restaurant or corporate business many people have jingles like McDonalds, Wendy’s, Subway, and Pizza Hut these people came up with there own jingles to sale there products to there target audience. Another thing you can do is sit down and write out an outline of the things you’re trying to sale than right and catchy but simple jingle that will stop your audience dead in there tracks. Many people hire writers for there jingles in some right there jingles themselves which will also save you more money that way also without going over your budget. You can say something like Dixie Professional Writing Service always on your side. You can say something like that but with more expression to it but just remember to be creative enough to advertise your business.";
+        }else if(nNews==3){
+            newPNews.innerHTML = "There are many things that are important to catalog design....Your images must be sharp and appealing. Your text and even the font you use for the text is important. The cover page design and the design of your catalog’s product pages all play important roles in designing a catalog that will bring in new customers and sales.However none of the things mentioned above are more important than the type of paper you will choose to print your catalog on. The “feel” of your catalog is important to your potential customers. The longer they have your catalog in their hands, the more product they will buy. In this article I will go over some of the many paper choices you have to print your catalog on.First you need to decide if the cover will be the same paper as the rest of your catalog. In most cases the cover will be of heavier weight then the interior pages.Second you will need to choose coated or uncoated paper for printing your catalog. Coated paper is for the glossier layouts and uncoated is more economical, but can still display your products well.So you have two things to consider first, “Will it be glossy paper or not?”, “Will my cover be the same weight as the interior pages of my catalog?”Once you have answered those two questions the next decision is what the weight of the paper will be. Most catalogs go with 60, 70, or 80 lb. weights. So it would be 60lb uncoated or coated, 70 lb. Coated or uncoated. 80 lb. Paper usually is only chosen for 80 lb. Weight and above. Also, not all paper is measured by weight. Some is measured by thickness or “points”.Now the above is really over-simplified. There are other types of special paper you can choose from. There is textured paper, photographic paper, and other more expensive paper to choose from if you are selling a high-end or expensive product.There are also other considerations when choosing the paper your catalog will be printed on. Such as the method of printing you are going to choose.If you are going to have your catalog printed on a hot-set web press, then you can choose uncoated or coated paper. The heat will dry the ink as it prints your catalog. If your printer uses a cold-set web press then the ink air-dries and is absorbed into the paper. A cold-set web press cannot print on coated paper.If your printer uses a sheet-fed press then you can choose from any of the high quality papers they have available. The same goes for printers that do digital printing.It will depend a lot on the printer you choose. You need to find out what type of paper is available and you will want to see samples of the paper to get a better idea of which one you want to print your catalog on. Also ask your printer to suggest the different types of paper they think will be the best for your catalog.Here is a list of some of the types of paper your printer may have available for you to choose from or may be able to order for you.Dull Coated Paper Matte Coated Paper Glossy Coated Paper Antique Paper Vellum Paper Wove Paper Smooth Paper Felt Paper Linen Paper Fiber-added Paper Laid Paper Parchment Four things determine the quality of the paper, opacity, brightness, finish, and ppi. (Pounds per inch) Brightness affects the contrast and brilliance of the paper. It affects how your text will stand out on the pages of your catalog. Opacity is how much your ink is visible from the back of the page. If a paper is too opaque, your text and images from one page will be seen through onto another page that your customer is trying to read. The finish affects the feel of the pages and also how your images will look. The lower grade finishes produce grainier images while high quality finishes sharpen the look of your images.Ask your printer to provide you with samples or swatch booklets and a price list for printing your catalog on the various types of paper before you decide. You know what your budget is and paper is only one of the expenses you have to consider.";
+        }
+        
+        newPNews.classList.toggle("newPNews");
+    
+        news.appendChild(newPNews);
+    }else{
+        document.querySelector(".newPNews").remove();
+    }
+}
